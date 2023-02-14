ac4e9ed92ac46d3c3e7bd4a8b846316e53b6b02f
JeannotMunganga <jeannotngalula@gmail.com>
2023-02-14 21:07:25 +0200

    REPL model implemented.

--- src/main/java/com/jmn/Main.java
+++ src/main/java/com/jmn/Main.java
@@ -1,7 +1,49 @@
 package com.jmn;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Thanks for using JLOX...");
+
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (; ; ) {
+            System.out.println("> ");
+            String line = reader.readLine();
+
+            if (line == null) break;
+
+            run(line);
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+
+        //TODO: Shouldn't this method be called execute because we know all the scanner does is return tokens?
+        List<Token> tokens = scanner.scanTokens();
     }
 }
